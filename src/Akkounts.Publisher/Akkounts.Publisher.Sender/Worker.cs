1318e08a7b1ec49ec28c01351d0f8de5baf7bf89
luc9volts <luc9volts@gmail.com>
2020-03-02 22:24:46 -0300

    Bootstrap of Sender Module

---
+++ src/Akkounts.Publisher/Akkounts.Publisher.Sender/Worker.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Akkounts.Publisher.Sender
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(3000, stoppingToken);
+            }
+            _logger.LogInformation("STOPPED at: {time}", DateTimeOffset.Now);
+        }
+    }
+}
