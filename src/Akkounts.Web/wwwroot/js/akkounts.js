83c8f472d3f402609f8310c2b671de1269be9c6b
luc9volts <luc9volts@gmail.com>
2020-03-29 17:54:55 -0300

    first attempt

--- src/Akkounts.Web/wwwroot/js/akkounts.js
+++ src/Akkounts.Web/wwwroot/js/akkounts.js
@@ -1,59 +1,150 @@
-﻿const width = 1200,
-    height = 800,
-    format = d3.format(",d"),
-    color = d3.scaleOrdinal(d3.schemeCategory20c),
-    circleRadiusScale = d3.scaleSqrt()
-        .domain([0, 100])
-        .range([0, 100]),
-    bubble = d3.pack()
-        .size([width, height])
-        .padding(6),
+﻿const width = 960,
+    height = 500,
+    bubbleDataState = [{ radius: 0, fixed: true }, { account: "ACC0100", balance: 200 }, { account: "ACC0950", balance: 250 }],
+    circleRadiusScale = d3.scale.sqrt()
+        .domain([0, 250])
+        .range([0, 50]),
+    nodes = bubbleDataState.map(d => {
+        d.radius = circleRadiusScale(d.balance);
+        d.fixed = false;
+        return d;
+    }),
+    root = nodes[0],
+    color = d3.scale.category10(),
     svg = d3.select("body").append("svg")
         .attr("width", width)
-        .attr("height", height)
-        .attr("class", "bubble"),
-    bubbleDataState = { children: [] };
+        .attr("height", height);
 
-const plot = (data, svg) => {
+root.radius = 0;
+root.fixed = true;
 
-    if (data.children.length <= 0) return;
+const plot = () => {
+    let circle = svg.selectAll("circle")
+        .data(nodes.slice(1));
 
-    let root = d3.hierarchy(data)
-        .sum(d => d.balance)
-        .sort((a, b) => b.balance - a.balance);
+    circle.exit().remove();
 
-    bubble(root);
+    circle.enter().append("circle")
+        .attr("r", d => d.radius)
+        .style("fill", (d, i) => color(i % 3));
 
-    let node = svg.selectAll(".node")
-        .data(root.children);
+    force.nodes(nodes);
+    tick();
+    force.start();
+}
 
-    node.exit().remove();
+const tick = () => {
+    var q = d3.geom.quadtree(nodes),
+        i = 0,
+        n = nodes.length;
 
-    node.enter().append("g")
-        .attr("class", "node")
-        .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+    while (++i < n) q.visit(collide(nodes[i]));
 
-    node.append("title")
-        .text(d => d.data.balance + ": " + format(d.value));
+    svg.selectAll("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+}
 
-    let sum = data.children.reduce((acc, elem) => acc + elem.balance, 0);
-    circleRadiusScale.domain([0, sum]);
-
-    node.append("circle")
-        .attr("r", d => circleRadiusScale(d.data.balance))
-        .style("fill", d => color(d.data.account));
-
-    node.append("text")
-        .attr("dy", ".3em")
-        .style("text-anchor", "middle")
-        .text(d => d.data.account.substring(0, d.r / 3));
+const collide = node => {
+    var r = node.radius + 16,
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+    return function (quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+            var x = node.x - quad.point.x,
+                y = node.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y),
+                r = node.radius + quad.point.radius;
+            if (l < r) {
+                l = (l - r) / l * .5;
+                node.x -= x *= l;
+                node.y -= y *= l;
+                quad.point.x += x;
+                quad.point.y += y;
+            }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    };
 };
 
-d3.select(self.frameElement).style("height", width + "px");
+const force = d3.layout.force()
+    .gravity(0.05)
+    .charge((d, i) => i ? 0 : -2000)
+    .nodes(nodes)
+    .size([width, height])
+    .on("tick", tick);
+
+plot(nodes, svg);
+
+window.setTimeout(() => {
+    nodes.push({ account: "ACC0500", balance: 150, radius: circleRadiusScale(150), fixed: false });
+    //nodes.pop();
+    plot(nodes, svg);
+}, 3000);
+
+window.setTimeout(() => {
+    nodes.pop();
+    plot(nodes, svg);
+}, 6000);
+
+// const width = 1200,
+//     height = 800,
+//     format = d3.format(",d"),
+//     color = d3.scaleOrdinal(d3.schemeCategory20c),
+//     circleRadiusScale = d3.scaleSqrt()
+//         .domain([0, 100])
+//         .range([0, 100]),
+//     bubble = d3.pack()
+//         .size([width, height])
+//         .padding(6),
+//     svg = d3.select("body").append("svg")
+//         .attr("width", width)
+//         .attr("height", height)
+//         .attr("class", "bubble"),
+//     bubbleDataState = { children: [] };
+
+// const plot = (data, svg) => {
+
+//     if (data.children.length <= 0) return;
+
+//     let root = d3.hierarchy(data)
+//         .sum(d => d.balance)
+//         .sort((a, b) => b.balance - a.balance);
+
+//     bubble(root);
+
+//     let node = svg.selectAll(".node")
+//         .data(root.children);
+
+//     node.exit().remove();
+
+//     node.enter().append("g")
+//         .attr("class", "node")
+//         .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+
+//     node.append("title")
+//         .text(d => d.data.balance + ": " + format(d.value));
+
+//     let sum = data.children.reduce((acc, elem) => acc + elem.balance, 0);
+//     circleRadiusScale.domain([0, sum]);
+
+//     node.append("circle")
+//         .attr("r", d => circleRadiusScale(d.data.balance))
+//         .style("fill", d => color(d.data.account));
+
+//     node.append("text")
+//         .attr("dy", ".3em")
+//         .style("text-anchor", "middle")
+//         .text(d => d.data.account.substring(0, d.r / 3));
+// };
+
+// d3.select(self.frameElement).style("height", width + "px");
 
 //connect with server via signalr
 const connection = new signalR.HubConnectionBuilder().withUrl("/Hubs/notificationHub").build();
-connection.start().then(() => alert("ok")).catch(err => console.error(err.toString()));
+//connection.start().then(() => alert("ok")).catch(err => console.error(err.toString()));
 
 const addBubble = txnInfo => {
     bubbleDataState.children.push(txnInfo);
